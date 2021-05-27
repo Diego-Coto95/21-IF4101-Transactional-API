dc93e4a7757c60696b6a0f13f00e0f29e8f950e3
Jenipher099 <jenn.mata.vrgs@gmailcom>
2021-05-26 20:59:09 -0600

    delete

--- 21-IF4101-Transactional-API/Controllers/NewsCommentsController.cs
+++ 21-IF4101-Transactional-API/Controllers/NewsCommentsController.cs
@@ -1,11 +1,9 @@
-﻿using System;
+﻿using _21_IF4101_Transactional_API.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using _21_IF4101_Transactional_API.Models.Entities;
 
 namespace _21_IF4101_Transactional_API.Controllers
 {
